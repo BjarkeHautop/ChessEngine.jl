3acf2bc5e70d729577111c7c6b305d095ded0790
Bjarke Hautop <bjarke.hautop@gmail.com>
2025-10-29 21:30:28 +0100

    Add test for show board

--- test/test_ui.jl
+++ test/test_ui.jl
@@ -1,12 +1,22 @@
 using OrbisChessEngine
 using Test
 
-@testset "display_board" begin
+@testset "plot_board" begin
     b = Board()
     g = Game()
 
     # Expect no error
     display_board = plot_board(b)
     display_game = plot_board(g)
+    @test true
+end
+
+@testset "show board" begin
+    b = Board()
+    g = Game()
+
+    # Expect no error
+    display_board = show(b)
+    display_game = show(g)
     @test true
 end
