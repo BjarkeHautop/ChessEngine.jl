a401913df8f5937a4d8765e69f0c17916202712a
Bjarke Hautop <bjarke.hautop@gmail.com>
2025-10-30 21:00:29 +0100

    fix test issue

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,14 +8,23 @@ Don't add your tests to runtests.jl. Instead, create files named
 
 The file will be automatically included inside a `@testset` with title "Title For My Test".
 =#
-for (root, dirs, files) in walkdir(@__DIR__)
-    for file in files
-        if isnothing(match(r"^test-.*\.jl$", file))
-            continue
-        end
-        title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
-        @testset "$title" begin
-            include(file)
+function run_tests(test_aqua::Bool)
+    for (root, dirs, files) in walkdir(@__DIR__)
+        for file in files
+            if isnothing(match(r"^test-.*\.jl$", file))
+                continue
+            end
+
+            if !test_aqua && file == "test-aqua.jl"
+                continue
+            end
+
+            title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
+            @testset "$title" begin
+                include(file)
+            end
         end
     end
 end
+
+run_tests(false)
